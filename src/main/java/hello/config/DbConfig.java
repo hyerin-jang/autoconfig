fb2d92d201eacb8a70f7280fe3e3e3af665c9c0a
Rin <hrjang92@gmail.com>
2024-07-17 15:29:27 +0900

    before auto configuration

---
+++ src/main/java/hello/config/DbConfig.java
@@ -0,0 +1,44 @@
+package hello.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.JdbcTransactionManager;
+import org.springframework.transaction.TransactionManager;
+
+import javax.sql.DataSource;
+
+@Slf4j
+@Configuration
+public class DbConfig {
+
+    private static final Logger log = LoggerFactory.getLogger(DbConfig.class);
+
+    @Bean
+    public DataSource dataSource() {
+
+        log.info("dataSource 빈 등록");
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setJdbcUrl("jdbc:h2:mem:test");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("");
+        return dataSource;
+    }
+
+    @Bean
+    public TransactionManager transactionManager() {
+        log.info("transactionManager 빈 등록");
+        return new JdbcTransactionManager(dataSource());
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        log.info("jdbcTemplate 빈 등록");
+        return new JdbcTemplate(dataSource());
+    }
+}
