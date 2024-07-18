e90243078b1dddd083af1cc8fdd9a84745c3b582
Rin <hrjang92@gmail.com>
2024-07-18 13:52:44 +0900

    auto configuration

--- src/test/java/hello/member/MemberRepositoryTest.java
+++ src/test/java/hello/member/MemberRepositoryTest.java
@@ -1,13 +1,11 @@
 package hello.member;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class MemberRepositoryTest {
